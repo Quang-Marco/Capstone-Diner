c1532d28c7550f069ae88ff79d6c9d955f6460fb
Quang - Marco <trannhanthienquang1803@gmail.com>
2024-05-01 14:44:46 +0700

    finish

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// Header overlay JS
+// Header overlay
 
 document.querySelector(".overlay_hide").onclick = function () {
   document.querySelector(".header_overlay").classList.toggle("hide");
@@ -8,9 +8,19 @@ document.querySelector(".overlay_close").onclick = function () {
   document.querySelector(".header_overlay").classList.toggle("hide");
 };
 
+// Dark Theme
+
 document.querySelector(".toggle").onclick = function () {
   document.getElementById("dark").classList.toggle("dark");
 };
+
+// Sidebar
+
+document.getElementById("sidebar_hidden").onclick = function () {
+  document.getElementById("sidebar").style.display = "none";
+};
+
+// Countup
 
 $(".counter").countUp({
   time: 5000,
